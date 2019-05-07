5eb2859aef3030194ef010baf2f65943c3ebca88
Husein Azzidix <huseinazidix12@gmail.com>
2019-05-08 03:09:53 +0700

    perbaikan beberapa kode, dan penjempurnaan angularjs

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ app.config(function($routeProvider, $locationProvider) {
 	$locationProvider.hashPrefix('');
 	$routeProvider
 	.when("/", {
-		template: "<h1>Welcome to Simple App</h1>"
-	})
-	.when("/zonaviz", {
-		templateUrl: "posts/view.html",
-		controller: "ctrlPrint"
+		templateUrl: "zonaviz/index.html",
+		controller: "ctrlMain"
 	})
 	.otherwise({
 		templateUrl: "404.html"
 	});
 });
 app.controller('ctrlMain', function($scope,$http) {
+	$scope.title = "ZonaViz";
     $scope.showMenu = function() {
         $('.navbar-left').css('transform', 'translateX(0%)');
     }
